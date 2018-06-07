22fcf1d3a2583bb1e2e4b9f7a670cd81e9c93077
eltonrufino <eltonrufino@hotmail.com>
2018-06-07 15:33:48 -0300

    alterando versao do spring boot, ajustando conversor de categoria para retorno imediato.

--- category-api/src/main/java/com/elton/app/converter/CategoryConverter.java
+++ category-api/src/main/java/com/elton/app/converter/CategoryConverter.java
@@ -1,4 +1,4 @@
-package  com.elton.app.converter;
+package com.elton.app.converter;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,23 +9,22 @@ import com.elton.app.model.Category;
 public class CategoryConverter {
 
 	public static Category fromDTO(final CategoryDTO dto) {
-		final Category model= new Category();
+		final Category model = new Category();
 		model.setId(dto.getCode());
 		model.setDescription(dto.getDescription());
 		return model;
 	}
 
 	public static CategoryDTO toDTO(final Category model) {
-		final CategoryDTO dto= new CategoryDTO();
+		final CategoryDTO dto = new CategoryDTO();
 		dto.setCode(model.getId());
 		dto.setDescription(model.getDescription());
 		return dto;
 	}
 
-	public static List<CategoryDTO> toDTO(final List<Category> listModel){
-		final List<CategoryDTO> results = listModel.stream().map(model -> {
+	public static List<CategoryDTO> toDTO(final List<Category> listModel) {
+		return listModel.stream().map(model -> {
 			return toDTO(model);
 		}).collect(Collectors.toList());
-		return results;
 	}
 }
