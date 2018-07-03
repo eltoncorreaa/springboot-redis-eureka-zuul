e4b0948da00f0fdab0caf2a9c5f344d870f517bd
resource <resource@10.20.33.77>
2018-07-03 16:28:21 -0300

    Ajustando sequence de categoria

--- category-api/src/main/java/com/elton/app/model/Category.java
+++ category-api/src/main/java/com/elton/app/model/Category.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -15,6 +16,7 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "CATEGORY")
+@SequenceGenerator(name = "SEQUENCE_CATEGORY", sequenceName = "SEQUENCE_CATEGORY")
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -23,7 +25,7 @@ public class Category implements Serializable {
 	private static final long serialVersionUID = -448760385229721893L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQUENCE_CATEGORY")
 	@Column(name = "ID_CATEGORY", precision = 12, scale = 0)
 	private Long id;
 
